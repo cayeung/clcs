1cd0c39ec93ce4552ba81f694175b3c4d07ad08a
Chloe Yeung <cayeung@stanford.edu>
2012-06-04 08:57:58 -0700

    seem to have fixed some basic cases

--- clcs-fast.cpp
+++ clcs-fast.cpp
@@ -4,6 +4,7 @@
 #include <limits>
 #include <stack>
 
+
 using namespace std;
 
 struct GridPoint {
@@ -13,19 +14,20 @@ struct GridPoint {
 
 struct forwardPath {
   forwardPath *child;
-  int x,y;
+  int cost, x,y;
 };
 
 forwardPath paths[2000];
-int matrix[4001][2000];
+int matrix[4000][2000];
 int aLen, bLen;
 string A, B;
-GridPoint new_grid[4000][2000];
+GridPoint new_grid[4001][2001];
 
 forwardPath * copyToPath(GridPoint *gp) {
   forwardPath *fp = new forwardPath;
   fp->x = gp->x;
   fp->y = gp->y;
+  fp->cost = gp->cost;
   return fp;
 }
 
@@ -35,18 +37,24 @@ forwardPath * reverseList(GridPoint *gp) {
   stack<GridPoint*> gpStack; 
   gpStack.push(gp);
   GridPoint *next = gp->parent;
-  while (next->parent != NULL) {
+  if (next == NULL) {
+    return copyToPath(gp);
+  }
+  while (next != NULL) {
     gpStack.push(next);
     next = next->parent; 
   }
   GridPoint *firstGP = gpStack.top();
   gpStack.pop();
   forwardPath *pathHead = copyToPath(firstGP);
-  for (forwardPath *fp  = pathHead; !gpStack.empty(); fp = fp->child) {
+  forwardPath *fp  = pathHead; 
+  while (!gpStack.empty()) {
     GridPoint *next_gp = gpStack.top(); 
     gpStack.pop();
     fp->child = copyToPath(next_gp);
+    fp = fp->child;
   }
+  fp->child = NULL; //initialize last child to NULL
   return pathHead;
 }
 
@@ -70,16 +78,16 @@ void initializeMatrix() {
 void initializelower(forwardPath* lower, int start) {
   
   forwardPath *head = lower;
-  for (int i = 0; i < bLen; i++) {
+  for (int i = 0; i < bLen+1; i++) {
     head->x = start;
     head->y = i;
     forwardPath *newChild=new forwardPath;
     head->child = newChild;
     head = head->child;
   }    
-  for (int j = start+1; j < aLen+start; j++) {
+  for (int j = start+1; j < aLen+start+1; j++) {
     head->x = j;
-    head->y = bLen-1;
+    head->y = bLen;
     forwardPath *newChild=new forwardPath;
     head->child = newChild;
     head = head->child;
@@ -90,15 +98,15 @@ void initializelower(forwardPath* lower, int start) {
 //if upper path does not exist, builds a path along the left boundary to the bottom
 void initializeUpper(forwardPath *upper, int start) {
   forwardPath *node = upper;
-  for (int k = start; k < aLen+start; k++) {
+  for (int k = start; k < aLen+start+1; k++) {
     node->x = k;
     node->y = 0;
     forwardPath *uchild =new forwardPath;
     node->child = uchild;
     node = node->child;
   }    
-  for (int j = 1; j < bLen; j++) {
-    node->x = aLen + start - 1;
+  for (int j = 1; j < bLen+1; j++) {
+    node->x = aLen + start;
     node->y = j;
     forwardPath *uchild =new forwardPath;
     node->child = uchild;
@@ -111,18 +119,25 @@ void initializeUpper(forwardPath *upper, int start) {
  modified dijkstra/BFS algorithm to find shortest path from each gridpoint
  */
 forwardPath singleShortestPath(int start, forwardPath* lowerPath, forwardPath* upperPath) {
-  
+  int upperbound;
+  int lowerbound;
+  int lbound;
+  int ubound;
   if (lowerPath->x == -1) {
-    initializelower(lowerPath, start);
+    lowerbound=0;
   }
+  //    initializelower(lowerPath, start);
+  //  }
   if (upperPath->x ==-1) {
-    initializeUpper(upperPath, start);
+    upperbound=0;
   }
-
+  //    initializeUpper(upperPath, start);
+  //  }
+  
   //implement way to make sure you dont check beyond upper/lower bound path
   //find shortest path from this grid point using a shortest path algorithm for directed acyclic graph
-  for (int i = start; i < aLen+start; i++) {
-    for (int j = 0; j< bLen; j++) {
+  for (int i = start; i < aLen+start+1; i++) {
+    for (int j = 0; j< bLen+1; j++) {
       GridPoint vertex;
       vertex.cost = numeric_limits<int>::max();
       vertex.parent = NULL;
@@ -131,80 +146,108 @@ forwardPath singleShortestPath(int start, forwardPath* lowerPath, forwardPath* u
       new_grid[i][j] = vertex;
     }
   }
-
+  
   new_grid[start][0].cost = 0;
   
   //traverses the linked list of the lower path to find the node in the path with the row start
   forwardPath l;
   forwardPath curr = *lowerPath;
-  
-  while (true) {
-    if (curr.x == start) {
-      l = curr;
-      break;
+  l=curr;
+  if (curr.x!=-1){
+    while (true) {
+      if (curr.child == NULL || curr.x == start) {  //added first condition
+        l = curr;
+        break;
+      }
+      curr = *(curr.child); 
     }
-    curr = *(curr.child);
   }
   
   //traverses the upper path and builds a dummy head 
   forwardPath upper = *upperPath;
   
   forwardPath trav;
-  trav.x = upperPath->x;
-  trav.y = 0;
-  trav.child = upperPath;
-  upper = trav;
-
+  if (upper.x!=-1){
+    trav.x = upperPath->x;
+    trav.y = 0;
+    trav.child = upperPath;
+    upper = trav;
+  }
+  
   //iterates through all the vertices from the start to the end between the upper and lower bounds. calculates the cost of the adjacent nodes and if the cost is greater than the cost of the current node + 1, replaces the cost. 
-  for (int i = start; i < aLen+start; i++) {
-    while (l.x == i) {
-      if (l.child !=NULL)
-        l = *(l.child);
-      else {
-        l.y=B.length();
+  for (int i = start; i < aLen+start+1; i++) {
+    if (l.x != -1){
+      while (l.x == i) {
+        if (l.child !=NULL)
+          l = *(l.child);
+        else {
+          l.y=B.length();
+        }
+      }
+      if (upper.x!=-1){
+        while (upper.x==i) {
+          if (upper.child == NULL) break; //added
+          upper = *(upper.child);
+        }
       }
     }
-    while (upper.x==i) {
-      upper = *(upper.child);
-    }
-    for (int j=upper.y; j<=l.y; j++) {
+    if (lowerbound==0)
+      lbound = bLen;
+    else 
+      lbound=l.y;
+    if (upperbound==0)
+      ubound = 0;
+    else
+      ubound=upper.y;
+    for (int j=ubound; j<=lbound; j++) {
       if (new_grid[i+1][j].cost > new_grid[i][j].cost + 1) {
         new_grid[i+1][j].cost = new_grid[i][j].cost + 1;
-        //cout << "one" <<i+1 << j << new_grid[i+1][j].cost << endl;
+        cout << "one" <<i+1 << j << new_grid[i+1][j].cost << endl;
         new_grid[i+1][j].parent = &new_grid[i][j];
       }
       if (new_grid[i][j+1].cost > new_grid[i][j].cost + 1) {
         new_grid[i][j+1].cost = new_grid[i][j].cost + 1;
         new_grid[i][j+1].parent = &new_grid[i][j];
-       // cout <<"two"<< i << j+1 << new_grid[i][j+1].cost << endl;
+        cout <<"two"<< i << j+1 << new_grid[i][j+1].cost << endl;
       }
-      if ((new_grid[i+1][j+1].cost > new_grid[i][j].cost + 1) && matrix[i+1][j+1] == 1) {
+      if ((new_grid[i+1][j+1].cost > new_grid[i][j].cost + 1) && matrix[i][j] == 1) {
         new_grid[i+1][j+1].cost = new_grid[i][j].cost + 1;
         new_grid[i+1][j+1].parent = &new_grid[i][j];
-       // cout <<"three"<< i+1 << j+1 << new_grid[i+1][j+1].cost << endl;
+        cout <<"three"<< i+1 << j+1 << new_grid[i+1][j+1].cost << endl;
       }
-      if (j==bLen-1) 
+      if (j==bLen) 
         break;
     }
-    if (l.child !=NULL)
-      l=*(l.child);
-    else 
-      l.x=l.x+1;
-    
-    if (upper.child !=NULL)
-      upper=*(upper.child);
-
+    if (l.x!=-1){
+      if (l.child !=NULL)
+        l=*(l.child);
+      else 
+        l.x=l.x+1;
+    }
+    if (upper.x!=-1){
+      if (upper.child !=NULL)
+        upper=*(upper.child);
+    }
     
   }
- 
+  
   //finds the shortest path and returns the forward version (from source to destination)
   GridPoint shortestPath;
-  shortestPath = new_grid[start+aLen-1][bLen-1];
-  cout << shortestPath.x << " " << shortestPath.y << endl;
-
+  shortestPath = new_grid[start+aLen][bLen];
+  //cout << shortestPath.x << " " << shortestPath.y << endl;
+  
   forwardPath *answer = reverseList(&shortestPath);
-
-  return *answer;
+  
+  
+  forwardPath *newHead = answer;
+  /*
+   while (answer->child != NULL) {
+   cout << answer->x << " " << answer->y << endl;
+   answer = answer->child;
+   }
+   exit(1); */
+  
+  return *newHead;
 }
 
 /*
@@ -212,7 +255,10 @@ forwardPath singleShortestPath(int start, forwardPath* lowerPath, forwardPath* u
  Fills paths array with shortest paths at each grid point. 
  */
 void findShortestPaths(int lower, int upper) {
-  if ((upper - 1) <= 1) return;
+  if ((upper - lower) <= 1){
+    paths[0]=singleShortestPath(0, &paths[1], &paths[0]);
+    return;
+  }
   int mid = (lower + upper) / 2;
   paths[mid] = singleShortestPath(mid, &paths[lower], &paths[upper]);
   findShortestPaths(lower, mid);
@@ -221,14 +267,16 @@ void findShortestPaths(int lower, int upper) {
 
 int getShortestPathLength() {
   int min = numeric_limits<int>::max();
-  for (int i = 0; i < 2000; i++) {
-    if (&paths[i] == NULL) break; 
-    int length = 0;
-    for (forwardPath *fp = &paths[i]; fp != NULL; fp = fp->child) {
-      length++; 
-    }
-    if (length < min) {
-      min = length;
+  for (int i = 0; i < aLen; i++) {
+    //if (paths[i].x == -1) break; //check &paths[i] == NULL or this? if paths[0] or something in the middle doesnt get assigned, everything stops...
+    if (paths[i].x != -1) {
+      int length = 0;
+      for (forwardPath *fp = &paths[i]; fp != NULL; fp = fp->child) {
+        length++; 
+      }
+      if (length < min) {
+        min = length;
+      }
     }
   }
   return min;
