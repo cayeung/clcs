de01cc25fd47fd5d8e7a2df858a0e8ef30aa4dd7
Chris Pak <chris.j.pak@gmail.com>
2012-06-04 16:45:40 -0700

    done with dijkstra

--- clcs.cc
+++ clcs.cc
@@ -16,8 +16,13 @@ typedef struct node {
     int x;
     int y;
     path *cur_path;
-    int lcs;
 } node;
+
+typedef struct output {
+    int *path_bound;
+    int shortest;
+} output;
+
 
 class pqueue {
 private:
@@ -35,6 +40,7 @@ public:
     void push(node *new_node);
     node *get_min();
     node *look_min();
+    int has_items();
 
 };
 
@@ -53,9 +59,21 @@ public:
     ~dg_matrix();
 
     bool has_diagonal(int x, int y);
-    int get_width();
+    int get_width() { return lenn; }
+    int get_height() { return lenm; }
+    int num_nodes();
 
 };
+
+bool 
+dg_matrix::has_diagonal(int x, int y)
+{
+    return graph_mat[get_location(x,y)];
+}
+
+int dg_matrix::num_nodes() { return lenm*lenn * 2; }
+
+int pqueue::has_items() { return count; }
 
 pqueue::pqueue(int max)
 {
@@ -133,7 +151,6 @@ node
     return list[0];
 }
 
-int dg_matrix::get_width (void) { return lenn; }
 
 int 
 dg_matrix::get_location (int x, int y)
@@ -175,6 +192,97 @@ dg_matrix::~dg_matrix()
     free(stringm);
     free(stringn);
     free(graph_mat);
+}
+
+output *
+construct_path (node *current, int width)
+{
+    path * cur_path = current->cur_path;
+
+    output *result = (output *)malloc(sizeof(output));
+    result->path_bound = (int*)malloc(width*sizeof(int));
+    memset(result->path_bound, 0, width*sizeof(int));
+    result->shortest = current->length;
+
+    while(cur_path) {
+        result->path_bound[cur_path->y] = cur_path->x;
+        cur_path = cur_path->prev;
+    }
+
+    return result;
+
+
+}
+
+output *
+single_shortest_path(dg_matrix *matrix, int offset, int *u_bound, int *l_bound)
+{
+    int width = matrix->get_width();
+    int height = matrix->get_height();
+
+    node *first = (node *)malloc(sizeof(node));
+    first->cur_path = (path *)malloc(sizeof(path));
+    first->x = offset;
+    first->y = 0;
+    first->length= 0;
+    first->cur_path->prev = NULL;
+    first->cur_path->x = offset;
+    first->cur_path->y = 0;
+
+    pqueue* node_list = new pqueue(matrix->num_nodes());
+
+    node_list->push(first);
+
+    while(node_list->has_items()) {
+        node *current = node_list->get_min();
+
+        int x = current->x;
+        int y = current->y;
+
+        if (current->x == offset+height/2-1 &&
+            current->y == width-1) {
+            output *s_path = construct_path(current, width);
+            return s_path;
+        }
+
+        if (x + 1 <= l_bound[y] && x + 1 <= offset - 1 + height/2) {
+            node *new_right = (node *)malloc (sizeof(node));
+            new_right->cur_path = (path *)malloc(sizeof(path));
+            new_right->x = x+1;
+            new_right->y = y;
+            new_right->cur_path->x = x+1;
+            new_right->cur_path->y = y;
+            new_right->cur_path->prev = current->cur_path;
+            new_right->length = current->length + 1;
+
+            node_list->push(new_right);
+        }
+        if (y + 1 <= u_bound[y] && y + 1 <= width -1) {
+            node *new_right = (node *)malloc (sizeof(node));
+            new_right->cur_path = (path *)malloc(sizeof(path));
+            new_right->x = x;
+            new_right->y = y+1;
+            new_right->cur_path->x = x;
+            new_right->cur_path->y = y+1;
+            new_right->cur_path->prev = current->cur_path;
+
+            node_list->push(new_right);
+        }
+        if (matrix->has_diagonal(x, y)) {
+            node *new_right = (node *)malloc (sizeof(node));
+            new_right->cur_path = (path *)malloc(sizeof(path));
+            new_right->x = x+1;
+            new_right->y = y+1;
+            new_right->cur_path->x = x+1;
+            new_right->cur_path->y = y+1;
+            new_right->cur_path->prev = current->cur_path;
+
+            node_list->push(new_right);
+        }   
+    }
+    //something is wrong
+    return NULL;
+    
 }
 
 int 
