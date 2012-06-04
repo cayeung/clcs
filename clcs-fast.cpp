014ef49c006c0c4e30125aaab2edfd174ebcbc46
Chloe Yeung <cayeung@stanford.edu>
2012-06-03 18:44:27 -0700

    reindented

--- clcs-fast.cpp
+++ clcs-fast.cpp
@@ -15,35 +15,35 @@ int matrix[4000][2000];
 string A, B;
 
 /*
-not sure if we need to initialize this shit as instance variables
-string A, B;
-GridPoint grid[2000][4000];
-string paths[2000];
-*/
+ not sure if we need to initialize this shit as instance variables
+ string A, B;
+ GridPoint grid[2000][4000];
+ string paths[2000];
+ */
 
 /* 
-initializes matrix and uses binary value to represent whether A and B match
-at specific spot 
-*/
+ initializes matrix and uses binary value to represent whether A and B match
+ at specific spot 
+ */
 void initializeMatrix() {
-/*
-  int a_length = A.length();
-  int b_length = B.length();
-  int matrix[4000][2000] = new int[a_length][b_length]; */ // cant use new on 2d array
+  /*
+   int a_length = A.length();
+   int b_length = B.length();
+   int matrix[4000][2000] = new int[a_length][b_length]; */ // cant use new on 2d array
   for (int i=0; i< (A.length()*2); i++) {
     for (int j=0; j< (B.length()); j++) {
       if (A[i]==B[j]) {
-	matrix[i][j]=1;
+        matrix[i][j]=1;
       } else { 
-	matrix[i][j]=0;
+        matrix[i][j]=0;
       }
     }
   }
 }
 
 /*
-  modified dijkstra/BFS algorithm to find shortest path from each gridpoint
-*/
+ modified dijkstra/BFS algorithm to find shortest path from each gridpoint
+ */
 string singleShortestPath(int start, GridPoint* lower, GridPoint* upper) {
   GridPoint new_grid[4000][2000];
   //implement way to make sure you dont check beyond upper/lower bound path
@@ -56,35 +56,35 @@ string singleShortestPath(int start, GridPoint* lower, GridPoint* upper) {
       new_grid[i][j] = vertex;
     }
   }
-
+  
   new_grid[start][0].cost = 0;
-
+  
   for (int i = start; i < A.length(); i++) {
     for (int j = 0; j < B.length(); j++) {
-        if (new_grid[i+1][j].cost < new_grid[i][j].cost + matrix[i+1][j]) {
-            new_grid[i+1][j].cost = new_grid[i][j].cost + matrix[i+1][j];
-            new_grid[i+1][j].parent = &new_grid[i][j];
-        }
-        if (new_grid[i][j+1].cost < new_grid[i][j].cost + matrix[i][j+1]) {
-            new_grid[i][j+1].cost = new_grid[i][j].cost + matrix[i][j+1];
-            new_grid[i][j+1].parent = &new_grid[i][j];
-        }
-        if (new_grid[i+1][j+1].cost < new_grid[i+1][j+1].cost + matrix[i+1][j+1]) {
-            new_grid[i+1][j+1].cost = new_grid[i+1][j+1].cost + matrix[i+1][j+1];
-            new_grid[i+1][j+1].parent = &new_grid[i][j];
-        }
-
+      if (new_grid[i+1][j].cost < new_grid[i][j].cost + matrix[i+1][j]) {
+        new_grid[i+1][j].cost = new_grid[i][j].cost + matrix[i+1][j];
+        new_grid[i+1][j].parent = &new_grid[i][j];
+      }
+      if (new_grid[i][j+1].cost < new_grid[i][j].cost + matrix[i][j+1]) {
+        new_grid[i][j+1].cost = new_grid[i][j].cost + matrix[i][j+1];
+        new_grid[i][j+1].parent = &new_grid[i][j];
+      }
+      if (new_grid[i+1][j+1].cost < new_grid[i+1][j+1].cost + matrix[i+1][j+1]) {
+        new_grid[i+1][j+1].cost = new_grid[i+1][j+1].cost + matrix[i+1][j+1];
+        new_grid[i+1][j+1].parent = &new_grid[i][j];
+      }
+      
     }
   }
   
-
+  
   return "";
 }
 
 /*
-161 Handout Pseudocode translated to c++.
-Fills paths array with shortest paths at each grid point. 
-*/
+ 161 Handout Pseudocode translated to c++.
+ Fills paths array with shortest paths at each grid point. 
+ */
 void findShortestPaths(int lower, int upper) {
   if ((upper - 1) <= 1) return;
   int mid = (lower + upper) / 2;
@@ -113,8 +113,8 @@ int main() {
 }
 
 /* 
-potential optimizations:
--if the number of diagonals that youve checked in your shortest path is already more than the number of diagonals left in your edge cases, then you can stop
--split as evenly as possible
--don't clear graph, just initialize a new one with nulls?
-*/
+ potential optimizations:
+ -if the number of diagonals that youve checked in your shortest path is already more than the number of diagonals left in your edge cases, then you can stop
+ -split as evenly as possible
+ -don't clear graph, just initialize a new one with nulls?
+ */
